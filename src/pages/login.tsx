f99de522f4bec3963bbefa54ea5021c983ed02b0
Alja Čekada <alja.cekada@gmail.com>
2024-03-27 20:23:33 -0700

    initialize project

---
+++ src/pages/login.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+
+// here is an example of a 'styled component'
+const Login = () => {
+  const Title = styled.h1`
+    font-size: 1.5em;
+    text-align: center;
+    color: #bf4f74;
+  `;
+
+  return (
+    // TODO: edit this page
+    <div>
+      <Title>Login</Title>
+    </div>
+  );
+};
+
+export default Login;
